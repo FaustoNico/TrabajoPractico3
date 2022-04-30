4f608bea01d2a5aca4fc5d621f11a5e5f893c610
Pino Fausto Nicolás <nicolasfausto5@gmail.com>
2022-04-30 10:59:18 -0300

    Creando etiqueta H1 en js

--- Programacion web dinámica/JavaScript/JavaScript.js
+++ Programacion web dinámica/JavaScript/JavaScript.js
@@ -1,7 +1,5 @@
 const elementoH1 = document.createElement('h1');
 elementoH1.textContent ="Hola profe widi";
 parent.appendChild(elementoH1);
-const elementoDiv = document.createElement('div');
-elementoDiv.innerHTML ='<h1>hola</h1>';
 body.appendChild(elementoH1);
 elementoDiv.appendChild(elementoH1);
